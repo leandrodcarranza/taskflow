f916aeb1c624f1d61a00b586950e3220bdef28ad
fgalan <nicolas.galan@mi.unc.edu.ar>
2026-04-09 17:58:29 -0300

    Implementada funcion para listar tareas

--- Taskflow/Taskflow/Program.cs
+++ Taskflow/Taskflow/Program.cs
@@ -6,5 +6,20 @@
         {
             Console.WriteLine("Hello, World!");
         }
+        static void ListarTarea(list<Tarea> lista) 
+        {
+            Console.WriteLine("Lista de Tareas");
+            if (lista.count == 0)
+            {
+                Console.WriteLine("No hay tareas registradas aun").
+            }
+            else 
+            {
+                foreach (var t in lista)
+                {
+                    Console.WriteLine($"{t.id}-{(.EstaCompletada ? "OK" : "..")}- {t.Descripcion}");
+                }
+            }
+        }
     }
 }
